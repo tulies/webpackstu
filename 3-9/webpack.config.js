df35b3de21ffe4e9919f19fe2109a1bad1f9e22c
王嘉炀 <346461062@qq.com>
2018-04-03 21:24:04 +0800

    '更新'

---
+++ 3-9/webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+module.exports = {
+    entry: {
+        app: './src/app.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader' //顺序是必要，放在后面的先处理
+                    }
+                ]
+            }
+        ]
+    }
+}
